170cd9b5e40ea933f318f56f7b828eb74e76ac52
Charles Lambert <charles@mbp-de-charles.lan>
2022-08-19 18:29:25 +0200

    add Number)

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
   try {
-    const genre = req.query.genres ? req.query.genres : "4,10,5,2,3,7,15";
+    const genre = Number(req.query.genres)
+      ? req.query.genres
+      : "4,10,5,2,3,7,15";
     const response = await axios.get(
       // `https://api.rawg.io/api/games?key=${process.env.API_KEY}&search=${req.query.search}`
       `https://api.rawg.io/api/games?key=${process.env.API_KEY}&search=${req.query.search}&page=${req.query.page}&page_size=${req.query.page_size}&genres=${genre}`
